99c4c7a8ba2846cf1ac27593fca7d941772424ef
Miad V <miadsoft@gmail.com>
2022-03-03 00:19:08 +0800

    topbar items

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Link from 'next/link';
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import { ThemeToggle } from './ThemeToggle';
 import { BWCompactLogo } from './Logo';
+import { topBarLinks } from '@/mockData';
+import Image from 'next/image';
 
 const TopBar: React.FC<{ classname?: string }> = ({ classname }) => {
   return (
@@ -20,135 +22,52 @@ const TopBar: React.FC<{ classname?: string }> = ({ classname }) => {
             </Link>
           </span>
           <nav className='h-full'>
-            <ul className='flex h-full items-center'>
-              <li className='flex h-full cursor-pointer items-center px-2 transition-all duration-75 hover:bg-gray-600'>
-                Fashion
-              </li>
-              <li className='flex h-full cursor-pointer items-center px-2 transition-all duration-75 hover:bg-gray-600'>
-                {'Home & Living'}
-              </li>
-              <Popover as='li' className='h-full'>
-                {({ open }) => (
-                  <>
-                    <Popover.Button className='relative flex h-full cursor-pointer items-center px-2 transition-all duration-75 hover:bg-gray-600'>
-                      {'Home & Living'}
-                      {open && (
-                        <span className='absolute bottom-0 left-[calc(50%_-_8px)]'>
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            height='8'
-                            viewBox='0 0 16 8'
-                            width='16'
-                          >
-                            <path
-                              d='M8 -1.49015e-08C8 -1.49015e-08 14.337 6.14491 16 8H0C1.66298 6.14492 8 -1.49015e-08 8 -1.49015e-08Z'
-                              fill='currentColor'
-                            ></path>
-                          </svg>
-                        </span>
-                      )}
-                    </Popover.Button>
-
-                    <Popover.Panel className='absolute inset-x-0 z-10 border-b border-gray-50 bg-white p-4 text-gray-800 shadow-2xl dark:border-gray-600 dark:bg-gray-700 dark:text-gray-50'>
-                      <div className='mx-auto max-w-5xl'>
-                        <div className='flex flex-wrap justify-between'>
-                          <ul>
-                            A
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            B
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            C
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            D
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            E
-                            <li>
-                              <a href='/analytics'>Analyticsaaaaaaaaa</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            F
-                            <li>
-                              <a href='/analytics'>Analyticsaaaaaaa</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            G
-                            <li>
-                              <a href='/analytics'>Analyticsaaaaaaaaa</a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                          <ul>
-                            H
-                            <li>
-                              <a href='/analytics'>
-                                Analyticsaaaaaaaaaaaaaaaaaaaaaa
-                              </a>
-                            </li>
-                            <li>
-                              <a href='/analytics'>Analytics</a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </>
-                )}
-              </Popover>
-            </ul>
+            <Popover.Group as='ul' className='flex h-full items-center'>
+              {Object.keys(topBarLinks).map((item, i) => (
+                <Popover as='li' className='h-full' key={i}>
+                  {({ open }) => (
+                    <>
+                      <Popover.Button className='relative flex h-full cursor-pointer items-center overflow-hidden truncate px-2 transition-all duration-75 hover:bg-gray-600'>
+                        {item}
+                        {open && (
+                          <span className='absolute bottom-0 left-[calc(50%_-_8px)]'>
+                            <svg
+                              xmlns='http://www.w3.org/2000/svg'
+                              height='8'
+                              viewBox='0 0 16 8'
+                              width='16'
+                            >
+                              <path
+                                d='M8 -1.49015e-08C8 -1.49015e-08 14.337 6.14491 16 8H0C1.66298 6.14492 8 -1.49015e-08 8 -1.49015e-08Z'
+                                fill='currentColor'
+                              ></path>
+                            </svg>
+                          </span>
+                        )}
+                      </Popover.Button>
+                      <Transition
+                        enter='transition duration-75 ease-out'
+                        enterFrom='opacity-0'
+                        enterTo='opacity-100'
+                        leave='transition duration-75 ease-out'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'
+                      >
+                        <Popover.Panel className='absolute inset-x-0 z-10 border-b border-gray-50 bg-white p-4 text-gray-800 shadow-2xl dark:border-gray-600 dark:bg-gray-600 dark:text-gray-50'>
+                          <div className='mx-auto max-w-5xl'>
+                            <div className='grid grid-cols-5 gap-4'>
+                              {topBarLinks[item].map((col, i) => {
+                                return <RenderColumns key={i} col={col} />;
+                              })}
+                            </div>
+                          </div>
+                        </Popover.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Popover>
+              ))}
+            </Popover.Group>
           </nav>
           <div className='ml-auto flex h-full items-center'>
             <button className='h-full px-4 font-semibold transition-all duration-75 hover:bg-gray-600'>
@@ -166,3 +85,43 @@ const TopBar: React.FC<{ classname?: string }> = ({ classname }) => {
 };
 
 export default TopBar;
+
+const RenderColumns: React.FC<{ col: typeof topBarLinks[number][number] }> = ({
+  col,
+}) => {
+  return (
+    <>
+      {col.map((parent) => {
+        return (
+          <ul key={parent.title} className='space-y-2'>
+            <Link href={parent.href}>
+              <a className='relative text-base font-semibold transition-all hover:text-teal-600 hover:underline dark:hover:text-teal-400'>
+                {parent.icon && (
+                  <span className='absolute -left-8'>
+                    <Image
+                      src={parent.icon}
+                      height={24}
+                      width={24}
+                      alt='icon'
+                    />
+                  </span>
+                )}
+                <span>{parent.title}</span>
+              </a>
+            </Link>
+
+            {parent.children.map((item) => (
+              <li key={item.title}>
+                <Link href={item.href}>
+                  <a className='text-sm transition-all hover:text-teal-600 hover:underline dark:text-gray-300 dark:hover:text-teal-400'>
+                    {item.title}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        );
+      })}
+    </>
+  );
+};
--- src/mockData.ts
+++ src/mockData.ts
@@ -9,7 +9,7 @@ export const categories = [
   },
   {
     title: "Women's Fashion",
-    thumbnail: '/images/categories/Womens_Fashion.png',
+    thumbnail: '/images/categories/Womens-Fashion.png',
   },
   {
     title: 'Free Items',
@@ -61,7 +61,7 @@ export const categories = [
   },
   {
     title: 'Kitchen & Appliances',
-    thumbnail: '/images/categories/kitchen_appliances.png',
+    thumbnail: '/images/categories/kitchen-appliances.png',
   },
   {
     title: 'Luxury',
@@ -71,297 +71,519 @@ export const categories = [
     title: 'Home & Furniture',
     thumbnail: '/images/categories/Furniture.png',
   },
+  {
+    title: 'Sports',
+    thumbnail: '/images/categories/Sports.png',
+  },
+  {
+    title: 'Textbooks',
+    thumbnail: '/images/categories/Text-Books.png',
+  },
+  {
+    title: 'Design & Craft',
+    thumbnail: '/images/categories/design-craft.png',
+  },
+  {
+    title: 'Auto Accessories',
+    thumbnail: '/images/categories/Car-Accessories.png',
+  },
+  {
+    title: 'Motorbikes',
+    thumbnail: '/images/categories/Motorbikes.png',
+  },
+  {
+    title: 'Cars',
+    thumbnail: '/images/categories/Cars.png',
+  },
+  {
+    title: 'Property',
+    thumbnail: '/images/categories/Property.png',
+  },
+  {
+    title: 'K-Wave',
+    thumbnail: '/images/categories/Kwave.png',
+  },
+  {
+    title: 'J-pop',
+    thumbnail: '/images/categories/Jpop.png',
+  },
+  {
+    title: 'Tickets & Vouchers',
+    thumbnail: '/images/categories/Ticket-Voucher.png',
+  },
+  {
+    title: 'Antiques',
+    thumbnail: '/images/categories/Vintage-Collectibles.png',
+  },
+  {
+    title: 'Pet Supplies',
+    thumbnail: '/images/categories/Pet-Supplies.png',
+  },
 ];
 
-export const topMenuLinks = {
-  Fashion: {
-    "Men's Fashion": [
+export const topBarLinks: Record<
+  string,
+  {
+    title: string;
+    href: string;
+    icon: string;
+    children: {
+      title: string;
+      href: string;
+    }[];
+  }[][]
+> = {
+  Fashion: [
+    //col-1
+    [
       {
-        title: 'Bags & Wallets',
+        title: "Men's Fashion",
         href: '/#',
+        icon: '/images/categories/Mens-Fashion.png',
+        children: [
+          {
+            title: 'Bags & Wallets',
+            href: '/#',
+          },
+          {
+            title: 'Footwear',
+            href: '/#',
+          },
+          {
+            title: 'Clothes',
+            href: '/#',
+          },
+          {
+            title: 'Accessories',
+            href: '/#',
+          },
+          {
+            title: 'Watches',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: 'Footwear',
+        title: "Women's Fashion",
         href: '/#',
+        icon: '/images/categories/Womens-Fashion.png',
+        children: [
+          {
+            title: 'Clothes',
+            href: '/#',
+          },
+          {
+            title: 'Bags & Wallets',
+            href: '/#',
+          },
+          {
+            title: 'Shoes',
+            href: '/#',
+          },
+          {
+            title: 'Accessories',
+            href: '/#',
+          },
+          {
+            title: 'Jewellery',
+            href: '/#',
+          },
+          {
+            title: 'Watches',
+            href: '/#',
+          },
+          {
+            title: 'Bridal Wear',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: 'Clothes',
+        title: 'Muslimah Fashion',
         href: '/#',
+        icon: '/images/categories/Muslimah-Fashion.png',
+        children: [
+          {
+            title: 'Tops',
+            href: '/#',
+          },
+          {
+            title: 'Bottoms',
+            href: '/#',
+          },
+          {
+            title: 'Two-piece',
+            href: '/#',
+          },
+          {
+            title: 'Dresses',
+            href: '/#',
+          },
+          {
+            title: 'Scarves',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: 'Accessories',
+        title: 'Health & Beauty',
         href: '/#',
+        icon: '/images/categories/Health-Beauty.png',
+        children: [
+          {
+            title: 'Skin, Bath, & Body',
+            href: '/#',
+          },
+          {
+            title: 'Hair Care',
+            href: '/#',
+          },
+          {
+            title: 'Makeup',
+            href: '/#',
+          },
+          {
+            title: "Men's Grooming",
+            href: '/#',
+          },
+          {
+            title: 'Perfumes, Nail Care, & Others',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: 'Watches',
+        title: 'Luxury',
         href: '/#',
-      },
-    ],
-    "Women's Fashion": [
-      {
-        title: 'Clothes',
-        href: '/#',
-      },
-      {
-        title: 'Bags & Wallets',
-        href: '/#',
-      },
-      {
-        title: 'Shoes',
-        href: '/#',
-      },
-      {
-        title: 'Accessories',
-        href: '/#',
-      },
-      {
-        title: 'Jewellery',
-        href: '/#',
-      },
-      {
-        title: 'Watches',
-        href: '/#',
-      },
-      {
-        title: 'Bridal Wear',
-        href: '/#',
-      },
-    ],
-    'Muslimah Fashion': [
-      {
-        title: 'Tops',
-        href: '/#',
-      },
-      {
-        title: 'Bottoms',
-        href: '/#',
-      },
-      {
-        title: 'Two-piece',
-        href: '/#',
-      },
-      {
-        title: 'Dresses',
-        href: '/#',
-      },
-      {
-        title: 'Scarves',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
-      },
-    ],
-    'Health & Beauty': [
-      {
-        title: 'Skin, Bath, & Body',
-        href: '/#',
-      },
-      {
-        title: 'Hair Care',
-        href: '/#',
-      },
-      {
-        title: 'Makeup',
-        href: '/#',
-      },
-      {
-        title: "Men's Grooming",
-        href: '/#',
-      },
-      {
-        title: 'Perfumes, Nail Care, & Others',
-        href: '/#',
-      },
-    ],
-    Luxury: [
-      {
-        title: 'Bags & Wallets',
-        href: '/#',
-      },
-      {
-        title: 'Apparel',
-        href: '/#',
-      },
-      {
-        title: 'Accessories',
-        href: '/#',
-      },
-      {
-        title: 'Watches',
-        href: '/#',
-      },
-    ],
-  },
-  'Home & Living': {
-    'Books & Stationery': [
-      {
-        title: 'Stationery',
-        href: '/#',
-      },
-      {
-        title: 'Books',
-        href: '/#',
-      },
-      {
-        title: "Children's Books",
-        href: '/#',
-      },
-      {
-        title: 'Comics & Manga',
-        href: '/#',
-      },
-      {
-        title: 'Magazines & Others',
-        href: '/#',
-      },
-    ],
-    Textbooks: [],
-    'Home & Furniture': [
-      {
-        title: 'Home Décor',
-        href: '/#',
-      },
-      {
-        title: 'Furniture',
-        href: '/#',
-      },
-      {
-        title: 'Gardening',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
+        icon: '/images/categories/Luxury.png',
+        children: [
+          {
+            title: 'Bags & Wallets',
+            href: '/#',
+          },
+          {
+            title: 'Apparel',
+            href: '/#',
+          },
+          {
+            title: 'Accessories',
+            href: '/#',
+          },
+          {
+            title: 'Watches',
+            href: '/#',
+          },
+        ],
       },
     ],
-    'Kitchen & Appliances': [],
-    'Babies & Kids': [
+  ],
+  'Home & Living': [
+    //col-1
+    [
       {
-        title: 'Strollers, Bags & Carriers',
+        title: 'Books & Stationery',
         href: '/#',
+        icon: '/images/categories/Books-Stationary.png',
+        children: [
+          {
+            title: 'Stationery',
+            href: '/#',
+          },
+          {
+            title: 'Books',
+            href: '/#',
+          },
+          {
+            title: "Children's Books",
+            href: '/#',
+          },
+          {
+            title: 'Comics & Manga',
+            href: '/#',
+          },
+          {
+            title: 'Magazines & Others',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: 'Toys & Walkers',
+        title: 'Textbooks',
         href: '/#',
+        icon: '/images/categories/Text-Books.png',
+        children: [],
       },
       {
-        title: "Girls' Apparel",
+        title: 'Home & Furniture',
         href: '/#',
+        icon: '/images/categories/Furniture.png',
+        children: [
+          {
+            title: 'Home Décor',
+            href: '/#',
+          },
+          {
+            title: 'Furniture',
+            href: '/#',
+          },
+          {
+            title: 'Gardening',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
       },
       {
-        title: "Boys' Apparel",
+        title: 'Kitchen & Appliances',
         href: '/#',
+        icon: '/images/categories/kitchen-appliances.png',
+        children: [],
       },
       {
-        title: 'Babies Apparel',
+        title: 'Babies & Kids',
         href: '/#',
-      },
-      {
-        title: 'Cots & Cribs',
-        href: '/#',
-      },
-      {
-        title: 'Nursing & Feeding',
-        href: '/#',
-      },
-      {
-        title: 'Maternity',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
-      },
-    ],
-  },
-  'Mobiles & Electronics': {
-    Electronics: [
-      {
-        title: 'Computers',
-        href: '/#',
-      },
-      {
-        title: 'TVs & Entertainment Systems',
-        href: '/#',
-      },
-      {
-        title: 'Audio',
-        href: '/#',
-      },
-      {
-        title: 'Computer Parts & Accessories',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
-      },
-    ],
-    'Mobile Phones & Tablets': [
-      {
-        title: 'iPhone',
-        href: '/#',
-      },
-      {
-        title: 'Android Phones',
-        href: '/#',
-      },
-      {
-        title: 'Tablets',
-        href: '/#',
-      },
-      {
-        title: 'Mobile & Tablet Accessories',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
-      },
-    ],
-  },
-  'Hobbies & Games': {
-    Photography: [],
-    'Pet Supplies': [],
-    'Design & Craft': [],
-    'Food & Drinks': [],
-    'Music & Media': [],
-    'Toys & Games': [],
-    Sports: [],
-    'Video Gaming': [],
-    'K-Wave': [],
-    'J-pop': [],
-    'Tickets & Vouchers': [],
-    Antiques: [],
-  },
-  'Cars & Property': {
-    Cars: [
-      {
-        title: 'Cars for Sale',
-        href: '/#',
-      },
-      {
-        title: 'Vehicle Rentals',
-        href: '/#',
-      },
-    ],
-    'Auto Accessories': [],
-    Property: [
-      {
-        title: 'For Sale',
-        href: '/#',
-      },
-      {
-        title: 'Rentals',
-        href: '/#',
-      },
-      {
-        title: 'Others',
-        href: '/#',
+        icon: '/images/categories/Babies-Kids.png',
+        children: [
+          {
+            title: 'Strollers, Bags & Carriers',
+            href: '/#',
+          },
+          {
+            title: 'Toys & Walkers',
+            href: '/#',
+          },
+          {
+            title: "Girls' Apparel",
+            href: '/#',
+          },
+          {
+            title: "Boys' Apparel",
+            href: '/#',
+          },
+          {
+            title: 'Babies Apparel',
+            href: '/#',
+          },
+          {
+            title: 'Cots & Cribs',
+            href: '/#',
+          },
+          {
+            title: 'Nursing & Feeding',
+            href: '/#',
+          },
+          {
+            title: 'Maternity',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
       },
     ],
-    Motorbikes: [],
-  },
+  ],
+  'Mobiles & Electronics': [
+    //col-1
+    [
+      {
+        title: 'Electronics',
+        href: '/#',
+        icon: '/images/categories/Electronics.png',
+        children: [
+          {
+            title: 'Computers',
+            href: '/#',
+          },
+          {
+            title: 'TVs & Entertainment Systems',
+            href: '/#',
+          },
+          {
+            title: 'Audio',
+            href: '/#',
+          },
+          {
+            title: 'Computer Parts & Accessories',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
+      },
+      {
+        title: 'Mobile Phones & Tablets',
+        href: '/#',
+        icon: '/images/categories/mobile-phones-tablets.png',
+        children: [
+          {
+            title: 'iPhone',
+            href: '/#',
+          },
+          {
+            title: 'Android Phones',
+            href: '/#',
+          },
+          {
+            title: 'Tablets',
+            href: '/#',
+          },
+          {
+            title: 'Mobile & Tablet Accessories',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
+      },
+    ],
+  ],
+  'Hobbies & Games': [
+    //col-1
+    [
+      {
+        title: 'Photography',
+        href: '/#',
+        icon: '/images/categories/Photography.png',
+        children: [],
+      },
+      {
+        title: 'Pet Supplies',
+        href: '/#',
+        icon: '/images/categories/Pet-Supplies.png',
+        children: [],
+      },
+      {
+        title: 'Design & Craft',
+        href: '/#',
+        icon: '/images/categories/Pet-Supplies.png',
+        children: [],
+      },
+      {
+        title: 'Food & Drinks',
+        href: '/#',
+        icon: '/images/categories/design-craft.png',
+        children: [],
+      },
+
+      {
+        title: 'Music & Media',
+        href: '/#',
+        icon: '/images/categories/Music-Media.png',
+        children: [],
+      },
+    ],
+    //col-2
+    [
+      {
+        title: 'Toys & Games',
+        href: '/#',
+        icon: '/images/categories/Toys-Games.png',
+        children: [],
+      },
+      {
+        title: 'Sports',
+        href: '/#',
+        icon: '/images/categories/Sports.png',
+        children: [],
+      },
+      {
+        title: 'Video Gaming',
+        href: '/#',
+        icon: '/images/categories/Video-Game.png',
+        children: [],
+      },
+      {
+        title: 'K-Wave',
+        href: '/#',
+        icon: '/images/categories/Kwave.png',
+        children: [],
+      },
+
+      {
+        title: 'J-pop',
+        href: '/#',
+        icon: '/images/categories/Jpop.png',
+        children: [],
+      },
+    ],
+    //col-3
+    [
+      {
+        title: 'Tickets & Vouchers',
+        href: '/#',
+        icon: '/images/categories/Ticket-Voucher.png',
+        children: [],
+      },
+      {
+        title: 'Antiques',
+        href: '/#',
+        icon: '/images/categories/Vintage-Collectibles.png',
+        children: [],
+      },
+    ],
+  ],
+  'Cars & Property': [
+    //col-1
+    [
+      {
+        title: 'Cars',
+        href: '/#',
+        icon: '/images/categories/Cars.png',
+        children: [
+          {
+            title: 'Cars for Sale',
+            href: '/#',
+          },
+          {
+            title: 'Vehicle Rentals',
+            href: '/#',
+          },
+        ],
+      },
+      {
+        title: 'Auto Accessories',
+        href: '/#',
+        icon: '/images/categories/Car-Accessories.png',
+        children: [],
+      },
+      {
+        title: 'Property',
+        href: '/#',
+        icon: '/images/categories/Property.png',
+        children: [
+          {
+            title: 'For Sale',
+            href: '/#',
+          },
+          {
+            title: 'Rentals',
+            href: '/#',
+          },
+          {
+            title: 'Others',
+            href: '/#',
+          },
+        ],
+      },
+      {
+        title: 'Motorbikes',
+        href: '/#',
+        icon: '/images/categories/Motorbikes.png',
+        children: [],
+      },
+    ],
+  ],
 };
